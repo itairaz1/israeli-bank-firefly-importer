4074aef9bd9806e75cbb0b00e716c135351cd7b1
Itai Raz <itay.raz@gmail.com>
2022-10-21 11:35:17 +0300

    Loading config from message log + bump

--- src/load-config.js
+++ src/load-config.js
@@ -7,4 +7,5 @@ const __dirname = dirname(__filename);
 
 const configFromEnvVarAsArray = process.env['CONFIG_PATH'] ? [process.env['CONFIG_PATH']] : [] ;
 const configDir = [...configFromEnvVarAsArray, __dirname + '/config/'].join(delimiter);
+console.log(`Loading config from ${configDir}`);
 config.util.extendDeep(config, config.util.loadFileConfigs(configDir));
