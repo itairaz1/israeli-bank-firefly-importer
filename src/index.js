02d637b8f3c6fabf1b26b830af25be8d49687b7c
Itai Raz <itay.raz@gmail.com>
2023-06-14 10:38:41 +0300

    refactor(log): Add startup log with app version [skip ci]

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@
 
 import config from 'nconf';
 import { schedule } from 'node-cron';
+import { readFile } from 'fs/promises';
 import loadConfig from './load-config.js';
 import doImport from './importer/index.js';
 import logger, { init as loggerInit } from './logger.js';
 import { init as fireFlyInit } from './firefly.js';
+
+const packageJsonContent = await readFile(new URL('../package.json', import.meta.url));
+const pkg = JSON.parse(packageJsonContent.toString());
 
 async function run() {
   try {
@@ -31,6 +35,8 @@ async function init() {
 
 try {
   await init();
+  logger()
+    .info({ version: pkg.version }, 'Starting Israeli Bank Firefly iii Importer');
   await run();
   const cron = config.get('cron');
   if (cron) {
